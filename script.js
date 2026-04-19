bb030555ad20311361e3d30f2d95c9aae1098bfd
pushpakkolya <kolyapushpak70@gmail.com>
2026-04-19 16:22:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function openEnvelope() {
   const envelope = document.getElementById("envelope");
   const content = document.getElementById("content");
+  const body = document.body;
 
   envelope.classList.add("open");
 
   setTimeout(() => {
+    body.classList.add("opened");
+  }, 800);
+
+  setTimeout(() => {
     document.querySelector(".envelope-container").style.display = "none";
     content.classList.remove("hidden");
+    content.classList.add("show");
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, 1600);
 }
